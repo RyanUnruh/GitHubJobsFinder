c2322b14c91bc227d2197a898ebbea7b62ff90bf
RyanUnruh <you@example.com>
2021-03-16 22:38:36 -0500

    Job component styling created

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,31 @@
 import React from "react";
-
+import { Card, Badge } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
 export default function Job({ job }) {
-  return <div>{job.title}</div>;
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} -{" "}
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
+            <Badge ariant="secondary">{job.location}</Badge>
+            <div style={{ wordbreak: "break-all" }}>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
